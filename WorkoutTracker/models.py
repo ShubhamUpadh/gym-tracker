ad4652140171ae57ceebd1ae0ea8421b2604e233
VIJAY YADAV <01shubhamupadhyay@gmail.com>
2024-04-15 02:33:34 +0530

    created models for date_workout_table

--- WorkoutTracker/models.py
+++ WorkoutTracker/models.py
@@ -13,3 +13,11 @@ class predefined_exercise_table(models.Model):
     exercise_name = models.CharField(max_length=100)
     major_muscle = models.CharField(max_length=50)
     exercise_type = models.CharField(max_length=50)
+
+class date_workout_table(models.Model):
+    id = models.AutoField(primary_key=True)
+    # links date_workout_table to predefined_exercise_table
+    exercise_id = models.ForeignKey(predefined_exercise_table,on_delete=models.CASCADE, related_name='workouts')
+    # links date_workout_table to user_table
+    user_id = models.ForeignKey(user_table,on_delete=models.CASCADE,related_name='workouts')               
+    workout_date = models.DateField()
