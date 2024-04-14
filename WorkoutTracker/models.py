31c395ff8c13459bdedc2bcf768c3460b4d63716
VIJAY YADAV <01shubhamupadhyay@gmail.com>
2024-04-15 02:44:29 +0530

    created models for set_detail_table, custom_exercise_table

--- WorkoutTracker/models.py
+++ WorkoutTracker/models.py
@@ -21,3 +21,20 @@ class date_workout_table(models.Model):
     # links date_workout_table to user_table
     user_id = models.ForeignKey(user_table,on_delete=models.CASCADE,related_name='workouts')               
     workout_date = models.DateField()
+
+class set_detail_table(models.Model):
+    id = models.AutoField(primary_key=True)
+    # links set_detail_table to date_workout_table
+    date_workout_id = models.ForeignKey(date_workout_table,on_delete=models.CASCADE, related_name="sets")
+    set_number = models.IntegerField()
+    weight = models.IntegerField()
+    repetition = models.IntegerField()
+    remarks = models.CharField(max_length=50)
+
+class custom_exercise(models.Model):
+    id = models.AutoField(primary_key=True)
+    # links custom_exercise to user_table
+    user_id = models.ForeignKey(user_table,on_delete=models.CASCADE, related_name="customs")
+    exercise_name = models.CharField(max_length=100)
+    major_muscle = models.CharField(max_length=50)
+    exercise_type = models.CharField(max_length=50)
